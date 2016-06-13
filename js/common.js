bec5aa74e79ba73b7f566f357a8c13c0314c129a
Evgeniy Beskrovnyy <e.beskrovnyy@gmail.com>
2016-06-13 23:39:52 +0400

    initial

---
+++ js/common.js
@@ -0,0 +1,43 @@
+
+$(function() {
+
+	//document.getElementById('svg-icons').innerHTML = SVG_ICONS;
+
+
+});
+
+
+
+function goToElement () {
+
+	var animationAllow = 'is-animate';
+	var openState = 'is-open';
+	var nav = $('.js-nav');
+	var navControl = $('.js-nav__control');
+	var navPhone = $('.js-nav__phone');
+	var navH = 40;
+
+	$(".js-goto").click(function(e) {
+
+		nav.removeClass(animationAllow + ' ' + openState);
+		navControl.removeClass(animationAllow + ' ' + openState);
+		navPhone.removeClass(openState);
+		$('body').removeClass(openState);
+
+		var self = $(this),
+			targetClass = self.data('goto');
+		    target = $(targetClass);
+
+		var speed = 1000;
+
+		 e.preventDefault();
+
+
+	    $('html, body').stop().animate({
+	        scrollTop: target.offset().top - (navH + 10)
+	        
+	    }, speed, 'easeInOutCubic');
+	});
+
+} // goToElement
+
