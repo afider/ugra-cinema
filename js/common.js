09a093f115b60a87e49c385a5f60f50971cb5413
Evgeniy Beskrovnyy <e.beskrovnyy@gmail.com>
2016-06-14 22:43:43 +0400

    slider is started

--- js/common.js
+++ js/common.js
@@ -3,6 +3,17 @@ $(function() {
 
 	//document.getElementById('svg-icons').innerHTML = SVG_ICONS;
 
+	$('.js-slider').bxSlider({
+		mode: 'fade',
+		minSlides: 1,
+		maxSlides: 1,
+		slideMargin: 0,
+		auto: true,
+		pause: 5000,
+		speed: 1000,
+		autoHover: true
+
+	});
 
 });
 
